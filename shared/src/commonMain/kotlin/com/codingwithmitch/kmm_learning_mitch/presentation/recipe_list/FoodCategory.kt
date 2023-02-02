39dc167cc6009d16b1b3dac86fd89dbcc446a451
majid2851 <majidbagheri2851@gmail.com>
2023-02-02 14:20:21 +0330

    Completed part 39==>OK

---
+++ shared/src/commonMain/kotlin/com/codingwithmitch/kmm_learning_mitch/presentation/recipe_list/FoodCategory.kt
@@ -0,0 +1,16 @@
+package com.codingwithmitch.kmm_learning_mitch.presentation.recipe_list
+
+enum class FoodCategory(val value: String)
+{
+    ERROR("error"),
+    CHICKEN("Chicken"),
+    BEEF("Beef"),
+    SOUP("Soup"),
+    DESSERT("Dessert"),
+    VEGETARIAN("Vegetarian"),
+    MILK("Milk"),
+    VEGAN("Vegan"),
+    PIZZA("Pizza"),
+    DONUT("Donut"),
+}
+
---
+++ shared/src/commonMain/kotlin/com/codingwithmitch/kmm_learning_mitch/presentation/recipe_list/FoodCategoryUtil.kt
@@ -0,0 +1,26 @@
+package com.codingwithmitch.kmm_learning_mitch.presentation.recipe_list
+
+class FoodCategoryUtil
+{
+    fun getAllFoodCategories(): List<FoodCategory>
+    {
+        return listOf(
+            FoodCategory.ERROR,
+            FoodCategory.CHICKEN,
+            FoodCategory.BEEF,
+            FoodCategory.SOUP,
+            FoodCategory.DESSERT,
+            FoodCategory.VEGETARIAN,
+            FoodCategory.MILK,
+            FoodCategory.VEGAN,
+            FoodCategory.PIZZA,
+            FoodCategory.DONUT
+        )
+    }
+
+    fun getFoodCategory(value: String): FoodCategory? {
+        val map = FoodCategory.values().associateBy(FoodCategory::value)
+        return map[value]
+    }
+
+}
