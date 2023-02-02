e6c724f44ff9a3629e8b575bd30628b4fa08d8c1
majid2851 <majidbagheri2851@gmail.com>
2023-02-02 18:22:06 +0330

    Completed part 44==>OK

--- Android_KMM_Learning/src/main/java/com/codingwithmitch/kmm_learning_mitch/android/presentation/recipe_detail/RecipeDetailScreen.kt
+++ Android_KMM_Learning/src/main/java/com/codingwithmitch/kmm_learning_mitch/android/presentation/recipe_detail/RecipeDetailScreen.kt
@@ -6,13 +6,17 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codingwithmitch.food2forkcompose.presentation.theme.AppTheme
+import com.codingwithmitch.kmm_learning_mitch.android.presentation.components.RECIPE_IMAGE_HEIGHT
 import com.codingwithmitch.kmm_learning_mitch.android.presentation.components.RecipeImgage
+import com.codingwithmitch.kmm_learning_mitch.android.presentation.recipe_detail.component.LoadingRecipeShimmer
 import com.codingwithmitch.kmm_learning_mitch.android.presentation.recipe_detail.component.RecipeView
 import com.codingwithmitch.kmm_learning_mitch.android.presentation.recipe_list.components.RecipeCard
 import com.codingwithmitch.kmm_learning_mitch.domain.model.Recipe
 import com.codingwithmitch.kmm_learning_mitch.presentation.recipe_detail.RecipeDetailEvents
 import com.codingwithmitch.kmm_learning_mitch.presentation.recipe_detail.RecipeDetailState
+import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterialApi::class)
 @Composable
@@ -23,13 +27,15 @@ fun RecipeDetailScreen(
 {
     AppTheme(displayProgressBar = state.isLoading)
     {
-        if (state.recipe==null && state.isLoading)
+        if (state.recipe==null && state.isLoading==true)
         {
-            //loading
-        }else if (state.recipe==null && state.isLoading==false)
+            LoadingRecipeShimmer(imageHeight = RECIPE_IMAGE_HEIGHT.dp)
+        }
+        else if (state.recipe==null && state.isLoading==false)
         {
-            //nothing to show
-        }else
+            LoadingRecipeShimmer(imageHeight = RECIPE_IMAGE_HEIGHT.dp)
+        }
+        else
         {
             RecipeView(recipe = state.recipe!!)
         }
